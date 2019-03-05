8eba3922524eca49625e1a40ca2a7541617a232f
DariaZielinska <dari.zielinska@gmail.com>
2019-03-05 11:20:43 +0100

    section top selling products

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ $(function() {
         $(".header_menu").slideToggle("slow");
     });
 
+    function generateProducts() {
+        let $product = $('<div class="page_product col-sm-12 col-md-6 col-lg-3">' +
+            '<span><i class="fas fa-shopping-basket"></i></span>' +
+            '<span><i class="far fa-heart"></i></span>' +
+            '</div>');
+        $('.products_container').append($product);
+    }
+
+    for (let i=1; i< 9; i++){
+        generateProducts();
+    }
+
 });
